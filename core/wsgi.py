993a55a81b01fe49b7725c6c6811433c58d34238
Frank Jane Partner <96851698+FrankJanePartner@users.noreply.github.com>
2023-10-07 21:06:25 +0100

    Update wsgi.py
    Signed-off-by: Frank Jane Partner <96851698+FrankJanePartner@users.noreply.github.com>

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,20 +1,19 @@
-"""
-WSGI config for core project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
-
 import os
 import sys
+from django.core.wsgi import get_wsgi_application
 
-path = '/home/myusername/JUSTGYM'
-if path not in sys.path:
-    sys.path.insert(0, path)
+# Add the directory containing your Django project to the Python path
+project_home = '/home/myusername/JUSTGYM'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
 
+# Activate your virtual environment
+activate_this = os.path.join(project_home, 'mysite-virtualenv/bin/activate_this.py')
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
+# Set the DJANGO_SETTINGS_MODULE to your project's settings module
 os.environ['DJANGO_SETTINGS_MODULE'] = 'core.settings'
 
-from django.core.wsgi import get_wsgi_application
+# Create the WSGI application
 application = get_wsgi_application()
