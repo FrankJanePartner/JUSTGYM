6763df8edaf1c789138fd24590ed3d159b6a26a5
Frank Jane Partner <96851698+FrankJanePartner@users.noreply.github.com>
2023-10-07 11:23:07 +0100

    Update views.py
    Signed-off-by: Frank Jane Partner <96851698+FrankJanePartner@users.noreply.github.com>

--- store/views.py
+++ store/views.py
@@ -4,18 +4,49 @@ from .models import Category, Product
 
 
 def product_all(request):
-    products = Product.objects.prefetch_related("product_image").filter(is_active=True)
-    return render(request, "store/index.html", {"products": products})
+    products = Product.products.all()
+    return render(request, 'home.html', {'products': products})
 
 
 def category_list(request, category_slug=None):
     category = get_object_or_404(Category, slug=category_slug)
-    products = Product.objects.filter(
-        category__in=Category.objects.get(name=category_slug).get_descendants(include_self=True)
-    )
-    return render(request, "store/category.html", {"category": category, "products": products})
+    products = Product.objects.filter(category=category)
+    context = {
+        'category': category,
+        'products': products
+    }
+    return render(request, 'shop.html', context)
 
 
 def product_detail(request, slug):
-    product = get_object_or_404(Product, slug=slug, is_active=True)
-    return render(request, "store/single.html", {"product": product})
+    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    return render(request, 'detail.html', {'product': product})
+
+def about(request):
+    return render(request, 'about.html')
+
+def errorPage(request):
+    return render(request, '404-page.html')
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+def orderComplete(request):
+    return render(request, 'order-complete.html')
+
+def policy(request):
+    return render(request, 'policy.html')
+
+# def home(request):
+#     products = Product.products.all()
+#     return render(request, 'home.html')
+
+# def shop(request, category_slug=None):
+#     category = get_object_or_404(Category, slug=category_slug)
+#     products = Product.objects.filter(category=category)
+#     context = {
+#         'category': category,
+#         'products': products
+#     }
+#     return render(request, 'shop.html', {'products': products})
+
