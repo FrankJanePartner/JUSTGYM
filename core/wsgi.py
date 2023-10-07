cab6e6d13483c37755b01ff4cb24ac98366e9889
Frank Jane Partner <96851698+FrankJanePartner@users.noreply.github.com>
2023-10-07 20:53:23 +0100

    Update wsgi.py
    Signed-off-by: Frank Jane Partner <96851698+FrankJanePartner@users.noreply.github.com>

--- core/wsgi.py
+++ core/wsgi.py
@@ -14,7 +14,7 @@ path = '/home/myusername/JUSTGYM'
 if path not in sys.path:
     sys.path.insert(0, path)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'core.settings'
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
